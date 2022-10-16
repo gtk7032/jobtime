121fa164baa8b81fcfa682b6eb6a3db27aabd7e7
gtk7032 <gtk7032@gmail.com>
2022-10-16 20:23:40 +0900

    wip

--- jobnet.py
+++ jobnet.py
@@ -4,6 +4,14 @@ import csv
 from datetime import datetime as dt
 
 
+class ReadingJoblogError(Exception):
+    pass
+
+
+class ReadingScheduleError(Exception):
+    pass
+
+
 class Jobnet:
 
     START_MSG = "ジョブネットが開始しました。"
@@ -16,32 +24,64 @@ class Jobnet:
         self.end = end
 
     @staticmethod
-    def create_jobnets_from_file(path: str) -> dict[str, Jobnet]:
+    def read_joblog(path: str) -> list[Jobnet]:
+
         jobnets = {}
 
-        with open(path, "r", encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                date = dt.strptime(row["log date"], "%Y/%m/%d %H:%M:%S.%f")
-                dictid = row["inner jobnet main id"]
-                jobid = row["jobnet id"]
-                msg = row["message"]
-                name = row["jobnet name"]
+        try:
+            with open(path, "r", encoding="utf-8") as f:
 
-                if msg == Jobnet.START_MSG:
-                    jobnets[dictid] = Jobnet(jobid, name, date)
+                reader = csv.DictReader(f)
 
-                elif msg == Jobnet.END_MSG and dictid in jobnets.keys():
-                    jobnets[dictid].end = date
+                for row in reader:
+                    date = dt.strptime(row["log date"], "%Y/%m/%d %H:%M:%S.%f")
+                    dictid = row["inner jobnet main id"]
+                    jobid = row["jobnet id"]
+                    msg = row["message"]
+                    name = row["jobnet name"]
+
+                    if msg == Jobnet.START_MSG:
+                        jobnets[dictid] = Jobnet(jobid, name, date)
+
+                    elif msg == Jobnet.END_MSG and dictid in jobnets.keys():
+                        jobnets[dictid].end = date
+
+        except:
+            raise ReadingJoblogError("An error occurred while reading the joblog file.")
 
         now = dt.now()
+
         for job in jobnets.values():
             if job.end is None:
                 job.end = now
 
-        return jobnets
+        return sorted(list(jobnets.values()), key=lambda job: job.start)
 
     @staticmethod
-    def show(jobnets: dict[str, Jobnet]):
-        for k, v in jobnets.items():
-            print(k, vars(v))
+    def read_schedule(path: str) -> list[Jobnet]:
+
+        schedules = []
+
+        try:
+            with open(path, "r", encoding="utf-8") as f:
+
+                reader = csv.DictReader(f)
+
+                for row in reader:
+                    jobid = row["jobid"]
+                    name = row["jobnm"]
+                    start = dt.strptime(row["start"], "%H:%M:%S")
+                    end = dt.strptime(row["end"], "%H:%M:%S")
+                    schedules.append(Jobnet(jobid, name, start, end))
+
+        except:
+            raise ReadingScheduleError(
+                "An error occurred while reading the schedule file."
+            )
+
+        return sorted(schedules, key=lambda s: s.start)
+
+    @staticmethod
+    def show(jobnets: list[Jobnet]):
+        for job in jobnets:
+            print(vars(job))
