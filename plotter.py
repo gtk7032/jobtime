0ded0d67971fd60050009774027f4641be87766e
gtk7032 <gtk7032@gmail.com>
2022-10-23 23:44:15 +0900

    wip

--- plotter.py
+++ plotter.py
@@ -23,12 +23,16 @@ class Plotter:
             ]
             fmtd[jobid].extend(
                 [
+                    # fix
                     {
-                        "min": Util.cvrt_to_hour(job.start),
+                        "min": Util.cvrt_to_hour(job.start)
+                        if job.start is not None and job.end is not None
+                        else 0.0,
                         "len": Util.cvrt_to_hour(job.end) - Util.cvrt_to_hour(job.start)
-                        if Util.cvrt_to_hour(job.end) - Util.cvrt_to_hour(job.start)
+                        if job.start is None and job.end is not None) and Util.cvrt_to_hour(job.end) - Util.cvrt_to_hour(job.start)
                         > (5 / 60)
-                        else (5 / 60),
+                        elif (job.start is not None and job.end is not None) and Util.cvrt_to_hour(job.end) - Util.cvrt_to_hour(job.start) <= (5/60) (5 / 60)
+                        else 0.0,
                     }
                     for job in joblist.values()
                 ]
@@ -36,7 +40,7 @@ class Plotter:
         return fmtd
 
     @staticmethod
-    def create_jobnet_barh(
+    def create_barh(
         fmtd_jobnets: dict[str, list[dict[str, float]]]
     ) -> Tuple[list[float], list[float]]:
         btms = []
@@ -48,24 +52,30 @@ class Plotter:
         return btms, lens
 
     @staticmethod
-    def create_schedule_barh(
-        fmtd_schedule: dict[str, list[dict[str, float]]], joblogids: list[str]
-    ) -> Tuple[list[float], list[float]]:
-        btms = []
-        lens = []
-        for i in range(len(next(iter(fmtd_schedule.values())))):
-            for jobid in joblogids:
-                btms.append(
-                    fmtd_schedule[jobid][i]["min"]
-                    if jobid in fmtd_schedule.keys()
-                    else 0.0
-                )
-                lens.append(
-                    fmtd_schedule[jobid][i]["len"]
-                    if jobid in fmtd_schedule.keys()
-                    else 0.0
-                )
-        return btms, lens
+    def complete(
+        target: dict[str, dict[str, Jobnet]], another: dict[str, dict[str, Jobnet]]
+    ):
+        for aid, aval in another.items():
+            if aid not in target.keys():
+                target[aid] = {
+                    "0": Jobnet(aid, "0", next(iter(aval.values())).name, None, None)
+                }
+
+    @staticmethod
+    def get_order(
+        jobnets: dict[str, dict[str, Jobnet]], schedule: dict[str, dict[str, Jobnet]]
+    ) -> list[str]:
+        s = sorted(
+            jobnets.items(),
+            key=lambda x: next(iter(x[1].values())).start
+            if next(iter(x[1].values())).start is not None
+            else next(iter(schedule[x[0]].values())).start,
+        )
+        return [i[0] for i in s]
+
+    @staticmethod
+    def align_order(target: dict[str, dict[str, Jobnet]], orderedkeys: list[str]):
+        return {key: target[key] for key in orderedkeys}
 
     @staticmethod
     def plot(
@@ -74,19 +84,22 @@ class Plotter:
         output: str,
         xrange: dict[str, str],
     ):
-        fmtd_jobnets = Plotter.format(jobnets)
-        # print(fmt_jobnets)
-        fmtd_schedule = Plotter.format(schedule)
-        # print(fmt_schedule)
 
-        jbtms, jlens = Plotter.create_jobnet_barh(fmtd_jobnets)
-        sbtms, slens = Plotter.create_schedule_barh(
-            fmtd_schedule, list(fmtd_jobnets.keys())
-        )
+        Plotter.complete(jobnets, schedule)
+        Plotter.complete(schedule, jobnets)
+
+        orderedkeys = Plotter.get_order(jobnets, schedule)
+        jobnets = Plotter.align_order(jobnets, orderedkeys)
+        schedule = Plotter.align_order(schedule, orderedkeys)
+
+        fmtd_jobnets = Plotter.format(jobnets)
+        fmtd_schedule = Plotter.format(schedule)
+
+        jbtms, jlens = Plotter.create_barh(fmtd_jobnets)
+        sbtms, slens = Plotter.create_barh(fmtd_schedule)
 
         lbls = [joblist[next(iter(joblist))].name for joblist in jobnets.values()]
-
-        y = np.arange(len(fmtd_jobnets.keys()))
+        y = np.arange(len(jobnets.keys()))
 
         plt.figure(figsize=(8.0, 6.0))
         plt.barh(y, jlens, left=jbtms, height=0.3, color="b", label="実行時間")
@@ -95,14 +108,11 @@ class Plotter:
         plt.yticks(y, lbls)
         plt.xlim([0, 9])
         plt.xticks(np.arange(0, 9 + 1, 1))
-
         plt.grid(color="gray", alpha=0.5)
         plt.rcParams["font.family"] = "IPAexGothic"
         # plt.savefig(_path.with_suffix(".png"), bbox_inches="tight")
         plt.show()
 
-        # print(btms)
-        # print("\n")
-        # print(lens)
-        # print("\n")
-        # print(lbls)
+        # jobnetが存在しない時もsheduleを表示する
+        # range
+        # sae image
